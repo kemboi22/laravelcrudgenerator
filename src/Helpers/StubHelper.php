c27e00e91ffa119d45b06acf5040a81a52e066c9
kemboi22 <kemboielvis22@gmail.com>
2024-07-24 02:26:05 +0300

    feat(stubs): added stubHelper

---
+++ src/Helpers/StubHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kemboielvis\LaravelCrudGenerator\Helpers;
+
+class StubHelper
+{
+    /**
+     * Get the content of a stub file
+     *
+     * @param string $type
+     * @return bool|string
+     */
+    public static function getStub(string $type): bool|string
+    {
+        return file_get_contents(__DIR__ . "/../Stubs/$type.stub");
+    }
+
+}
