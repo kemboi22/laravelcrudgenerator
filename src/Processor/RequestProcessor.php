70fba41d7e5b8a81777e0a7558b386e1e8a51dc1
kemboi22 <kemboielvis22@gmail.com>
2024-07-24 23:49:43 +0300

    feat(processors): few changes in RequestProcessor.php

--- src/Processor/RequestProcessor.php
+++ src/Processor/RequestProcessor.php
@@ -2,7 +2,92 @@
 
 namespace Kemboielvis\LaravelCrudGenerator\Processor;
 
+use Kemboielvis\LaravelCrudGenerator\Helpers\MigrationHelper;
+use Kemboielvis\LaravelCrudGenerator\Helpers\ModelHelper;
+use Kemboielvis\LaravelCrudGenerator\Helpers\StubHelper;
+
 class RequestProcessor
 {
+    /**
+     * Generate a request
+     *
+     * @param string $modelName
+     * @return void
+     */
+    public function generateRequest(string $modelName): void
+    {
+        $stub = StubHelper::getStub("Request");
+        $stubGetContent = file_get_contents($stub);
+        $requestStoreContent = $this->replacePlaceHolders($stubGetContent, $modelName, "Store");
+        if ($this->saveRequest($modelName, $requestStoreContent, "Store")) {
+            echo "Store Request created successfully";
+        }
+        $requestUpdateContent = $this->replacePlaceHolders($stubGetContent, $modelName, "Update");
+        if ($this->saveRequest($modelName, $requestUpdateContent, "Update")) {
+            echo "Update Request created successfully";
+        }
+
+    }
+    /**
+     * Replace placeholders in the request stub
+     *
+     * @param string $stubContent
+     * @param string $modelName
+     * @return string
+     */
+
+    public function getAttributes(string $modelName): array
+    {
+        // Instantiate Model class
+        $modelInstance = ModelHelper::instantiateModelClass($modelName);
+        // Get the fillables
+        $fillables = ModelHelper::getFillables($modelInstance);
+        // Get Migration Content
+        $migrationContent = MigrationHelper::getMigrationDetailsForModel($modelName);
+
+        // Define rules
+        $rules = [];
+
+        // Loop through fillables
+        foreach ($fillables as $fillable) {
+            $rules[$fillable] = 'required';
+        }
+
+        // Loop through migration content
+        foreach ($migrationContent as $migration) {
+            foreach ($migration as $column) {
+                // If the column is already in rules (from fillables), merge required/nullable condition
+                if (isset($rules[$column['name']])) {
+                    $rules[$column['name']] = $column['required'] ? 'required' : 'nullable';
+                } else {
+                    $rules[$column['name']] = $column['required'] ? 'required' : 'nullable';
+                }
+            }
+        }
+
+        return $rules;
+    }
+
+
+    public function replacePlaceHolders(string $stubContent, string $rules, string $type): array|string
+    {
+        //Replace Rules placeholder {{ rules }}
+        return str_replace(['{{ rules }}', '{{ type }}'], [$rules, $type], $stubContent);
+    }
+
+    public function saveRequest(string $modelName, string $requestContent, string $type): bool|int
+    {
+        // Get request path
+        $requestPath = app_path('Http/Requests');
+        // Ensure the directory exists
+        if (!is_dir($requestPath) && !mkdir($requestPath, 0755, true) && !is_dir($requestPath)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $requestPath));
+        }
+        // Get request file name
+        $requestFileName = $type.$modelName . 'Request.php';
+        // Get request file
+        $requestFile = $requestPath . '/' . $requestFileName;
+        return file_put_contents($requestFile, $requestContent);
+    }
 
 }
